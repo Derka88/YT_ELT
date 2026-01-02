489fb6df107159f77d759a019a2d6c217c0b3e1f
Abdoul Kader Mamoudou <kadermamoudou88@gmail.com>
2026-01-02 21:35:28 +0100

    add function get_video_ids

--- video_stats.py
+++ video_stats.py
@@ -27,10 +27,47 @@ def get_playlist_id():
     
     except requests.exceptions.RequestException as e:
         raise e
+
+def get_video_ids(playlist_id):
+    
+    video_ids = []
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=50&playlistId={playlist_id}&key={API_KEY}"    
+    page_token = None
+
+    try :
+        while True:
+            url = base_url
+
+            if page_token:
+                url += f"&pageToken={page_token}" 
+                
+            response = requests.get(url)
+            
+            response.raise_for_status()
+            
+            data = response.json()
+
+            for item in data.get('items', []) :
+                video_id = item["contentDetails"]["videoId"]
+                video_ids.append(video_id)
+            
+            page_token = data.get('nextPageToken')
+
+            if not page_token:
+                break
+        
+        return video_ids
+
+    except requests.exceptions.RequestException as e:
+        raise e
     
 if __name__ == '__main__' : 
     print('get_playlist_id will be executed')
-    get_playlist_id()
+    
+    playlist_id = get_playlist_id()
+    
+    video_ids = get_video_ids(playlist_id)
+
 else :
     print("get_playlist_id won't be executed")
 
