fee4098bf523e19c3133faf6b26b61d79bce24a1
Abdoul Kader Mamoudou <kadermamoudou88@gmail.com>
2026-01-03 02:20:48 +0100

    add data utils and data loafding files

--- dags/api/video_stats.py
+++ dags/api/video_stats.py
@@ -1,15 +1,19 @@
 import requests
 import json
-import os
 from datetime import date
 
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
+# load_dotenv(dotenv_path="./.env")
 
-load_dotenv(dotenv_path="./.env")
-API_KEY = os.getenv('API_KEY')
-CHANNEL_HANDLE = os.getenv("MrBeast") 
+from airflow.decorators import task
+from airflow.models import Variable
+
+API_KEY = Variable.get('API_KEY')
+CHANNEL_HANDLE = Variable.get("CHANNEL_HANDLE") 
 maxResults = 50
 
+@task
 def get_playlist_id():
     try: 
 
@@ -30,6 +34,7 @@ def get_playlist_id():
     except requests.exceptions.RequestException as e:
         raise e
 
+@task
 def get_video_ids(playlist_id):
     
     video_ids = []
@@ -64,8 +69,7 @@ def get_video_ids(playlist_id):
         raise e
 
 
-
-
+@task
 def extract_video_data(video_ids):
     extracted_data = []
 
@@ -108,6 +112,7 @@ def extract_video_data(video_ids):
     except requests.exceptions.RequestException as e:
         raise e
 
+@task
 def save_to_json(extracted_data):
     file_path = f"./data/YT_data_{date.today()}.json"
 
