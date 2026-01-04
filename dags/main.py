d1d0b8d4f34c67c2974a61298ec6732b7fea0580
Abdoul Kader Mamoudou <kadermamoudou88@gmail.com>
2026-01-04 18:49:18 +0100

    add transformation functions

--- dags/main.py
+++ dags/main.py
@@ -3,6 +3,8 @@ import pendulum
 from datetime import datetime,timedelta
 from api.video_stats import get_playlist_id,get_video_ids,extract_video_data,save_to_json
 
+from datawarehouse.dwh import staging_table, core_table
+
 #define the local timezone
 local_tz = pendulum.timezone("Europe/Paris")
 
@@ -34,3 +36,19 @@ with DAG(
 
     #Define dependencies 
     playlist_id >> video_ids >> extract_data >> save_to_json_task
+
+# DAG 2: update_db
+with DAG(
+    dag_id="update_db",
+    default_args=default_args,
+    description="DAG to process JSON file and insert data into both staging and core schemas",
+    catchup=False,
+    schedule=None,
+) as dag_update:
+
+    # Define tasks
+    update_staging = staging_table()
+    update_core = core_table()
+
+    #Define dependencies 
+    update_staging >> update_core 
